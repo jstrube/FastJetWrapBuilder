18c7caa19cc9f6c09d2cbd966c0d432a1ce137c8
Jan Strube <jan.strube@gmail.com>
2018-09-15 19:43:28 -0700

    fixing the build script

--- .ci/build_tarballs.jl
+++ .ci/build_tarballs.jl
@@ -2,10 +2,12 @@
 # `julia build_tarballs.jl --help` to see a usage message.
 using BinaryBuilder
 
-# Collection of sources required to build LCIOWrapBuilder
+name = "FastJetWrapBuilder"
+
+# Collection of sources required to build Fjwbuilder
 sources = [
-    "https://github.com/jstrube/LCIOWrapBuilder.git" =>
-    "451f42e9b3daf41156db502ee45ee8884b77efd2",
+    "https://github.com/jstrube/FastJetWrapBuilder.git" =>
+    "0b7d7155f322559fea220d5dc95c09867d127d75",
 ]
 
 # Bash recipe for building across all platforms
@@ -17,10 +19,11 @@ function getscript(version)
 	curl -L "https://github.com/JuliaPackaging/JuliaBuilder/releases/download/$version/julia-$version-\$target.tar.gz" | tar -zx --strip-components=1
 	cd \$WORKSPACE/srcdir
 	mkdir build && cd build
-	cmake -DCMAKE_INSTALL_PREFIX=\$prefix -DCMAKE_TOOLCHAIN_FILE=/opt/\$target/\$target.toolchain -DJulia_ROOT=\$Julia_ROOT ../LCIOWrapBuilder/
+	cmake -DCMAKE_INSTALL_PREFIX=\$prefix -DCMAKE_TOOLCHAIN_FILE=/opt/\$target/\$target.toolchain -DJulia_ROOT=\$Julia_ROOT ../FastJetWrapBuilder/
 	VERBOSE=ON cmake --build . --config Release --target install
 	"""
 end
+# Bash recipe for building across all platforms
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
@@ -31,13 +34,14 @@ platforms = [
 
 # The products that we will ensure are always built
 products(prefix) = [
-    LibraryProduct(prefix, "liblciowrap", :lciowrap)
+    LibraryProduct(prefix, "libfastjetwrap", :libfastjetwrap)
 ]
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-	"https://github.com/JuliaInterop/libcxxwrap-julia/releases/download/v0.4.0/build_libcxxwrap-julia-1.0.v0.4.0.jl"
-	"https://github.com/jstrube/LCIOBuilder/releases/download/v2.12.1/build_LCIOBuilder.v2.12.1.jl"
+    "https://github.com/jstrube/FastJetBuilder/releases/download/v3.3.1/build_FastJetBuilder.v0.1.0.jl",
+    "https://github.com/JuliaInterop/libcxxwrap-julia/releases/download/v0.4.0/build_libcxxwrap-julia-1.0.v0.4.0.jl",
+    "https://github.com/JuliaPackaging/JuliaBuilder/releases/download/1.0.0/build_Julia.v1.0.0.jl"
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
@@ -45,5 +49,5 @@ version_number = get(ENV, "TRAVIS_TAG", "")
 if version_number == ""
     version_number = "v0.99"
 end
-build_tarballs(ARGS, "LCIOWrapBuilder", VersionNumber(version_number), sources, getscript("1.0.0"), platforms, products, dependencies)
+build_tarballs(ARGS, name, VersionNumber(version_number), sources, getscript("1.0.0"), platforms, products, dependencies)
 
