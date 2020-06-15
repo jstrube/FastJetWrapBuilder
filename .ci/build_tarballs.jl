de21f03e874f91da2f3db5431603d172e2c004a6
Jan Strube <jan.strube@gmail.com>
2020-06-14 17:10:58 -0700

    build script update

--- .ci/build_tarballs.jl
+++ .ci/build_tarballs.jl
@@ -4,27 +4,18 @@ using BinaryBuilder
 using Pkg
 
 name = "FastJet_Julia_Wrapper"
-version = v"0.7.2"
+version = v"0.8.0-alpha4"
 
 # Collection of sources required to build Fjwbuilder
 sources = [
 	DirectorySource("FastJet_Julia_Wrapper"),
-	ArchiveSource("https://julialang-s3.julialang.org/bin/linux/x64/1.3/julia-1.3.1-linux-x86_64.tar.gz", "faa707c8343780a6fe5eaf13490355e8190acf8e2c189b9e7ecbddb0fa2643ad"; unpack_target="julia-x86_64-linux-gnu"),
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
-case "$target" in
-	arm-linux-gnueabihf|x86_64-linux-gnu)
-        Julia_PREFIX=${WORKSPACE}/srcdir/julia-$target/julia-1.3.1
-        ;;
-    x86_64-apple-darwin14|x86_64-w64-mingw32)
-        Julia_PREFIX=${WORKSPACE}/srcdir/julia-$target/juliabin
-        ;;
-esac
 cd ${WORKSPACE}/srcdir
 mkdir build && cd build
-cmake -DJulia_PREFIX=${Julia_PREFIX} -DCMAKE_INSTALL_PREFIX=${prefix} -DCMAKE_FIND_ROOT_PATH=${prefix} -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release ..
+cmake -DJulia_PREFIX=${prefix} -DCMAKE_INSTALL_PREFIX=${prefix} -DCMAKE_FIND_ROOT_PATH=${prefix} -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release ..
 VERBOSE=ON cmake --build . --config Release --target install
 install_license $WORKSPACE/srcdir/LICENSE.md
 """
@@ -32,9 +23,8 @@ install_license $WORKSPACE/srcdir/LICENSE.md
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = Platform[
-    Linux(:x86_64, libc=:glibc)
+    Linux(:x86_64; libc=:glibc, compiler_abi=CompilerABI(cxxstring_abi=:cxx11))
 ]
-platforms = expand_cxxstring_abis(platforms)
 
 # The products that we will ensure are always built
 products = [
@@ -43,10 +33,10 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-	Dependency("libcxxwrap_julia_jll"),
-	Dependency("FastJet_jll")
-	# Dependency(PackageSpec(;name="FastJet_jll", url="https://github.com/jstrube/FastJet_jll.jl"))
+	Dependency(PackageSpec(name="libcxxwrap_julia_jll", version=v"0.8")),
+	Dependency("FastJet_jll"),
+    BuildDependency(PackageSpec(name="Julia_jll", version=v"1.4.1"))
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version=v"9")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version=v"7")
